65a9f6f5199a6b46fd3b64cb3f5ad1f0d1e61b94
VVVova <65650034+VVVova@users.noreply.github.com>
2022-11-15 20:40:54 +0200

    Initial Commit

---
+++ TP/TPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TPApp.swift
+//  TP
+//
+//  Created by Developer on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
